1a0ddea5f34ba5345722fd0ebd735048de6cd578
Jeff <you@example.com>
2015-04-09 09:31:06 -0600

    Separate ir-form from home template; create partials folder for html that is appended to template, rather than main app frame

--- views/home.js
+++ views/home.js
@@ -5,7 +5,11 @@ home = {
 
 	init: function() {
 
-		
+		// Get it form html, append to #homepage-form div
+		$.get('templates/partials/ir-form.html', function(html) {
+				
+			$('#homepage-form').append(html);
+		});
 	}
 };
 
