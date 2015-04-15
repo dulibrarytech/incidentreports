64bfb6a9e95794e94e084b79c93125f4a6f3aa5f
Jeff <you@example.com>
2015-04-15 11:50:26 -0600

    Begin dashboard construct, table header ok, uncentered; add top margins

--- views/dashboard.js
+++ views/dashboard.js
@@ -1,6 +1,6 @@
 dashboard = {
 
-	element: "#content",
+	element: "#reports-table",
 	template: "dashboard",
 
 	init: function(tableData) {
@@ -11,7 +11,12 @@ dashboard = {
 
 	construct: function(tableData) {
 
-		//alert("C " + tableData[0][0]);
+		var tableString = "";
+
+		// Header
+		tableString += "<th width='5%'>Details</th><th width='6%'>FollowUp</th><th width='5%'>Tracking Number</th><th width='10%'>Date of Report</th><th width='10%'>Date of Offense</th><th width='10%'>Report Completed By</th><th width='10%'>Department</th><th width='10%'>Nature of Offense</th><th width='34%'>Offense Narrative</th>";
+
+		$(this.element).append(tableString);
 	}
 };
 
