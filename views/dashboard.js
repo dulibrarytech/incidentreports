ef867c88f01e5cb741a1c40059342338c1372d0c
Jeff <you@example.com>
2015-04-16 16:30:06 -0600

    Separate table header

--- views/dashboard.js
+++ views/dashboard.js
@@ -12,11 +12,13 @@ dashboard = {
 
 	construct: function(tableData) {
 
-		var tableString = "<tbody>";
+		var tableString = "";
 		var truncatedOffenseNarrative;
 
 		// Header
+		tableString += '<thead>';
 		tableString += '<tr><th width="5%">Details</th><th width="6%">FollowUp</th><th width="5%">Tracking Number</th><th width="10%">Date of Report</th><th width="10%">Date of Offense</th><th width="10%">Report Completed By</th><th width="10%">Department</th><th width="10%">Nature of Offense</th><th width="34%">Offense Narrative</th></tr>';
+		tableString += '</thead><tbody>';
 
 		var maxLength = this.truncLength;
 		$.each(tableData, function(index, value) {
