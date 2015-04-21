eb26a135745d25b759716c7db2a9f2836ad45d22
Jeff <you@example.com>
2015-04-21 15:38:21 -0600

    Remove 'follow up' column from reports table

--- views/dashboard.js
+++ views/dashboard.js
@@ -17,7 +17,7 @@ dashboard = {
 
 		// Header
 		tableString += '<thead>';
-		tableString += '<tr><th width="5%">Details</th><th width="6%">FollowUp</th><th width="5%">Tracking Number</th><th width="10%">Date of Report</th><th width="10%">Date of Offense</th><th width="10%">Report Completed By</th><th width="10%">Department</th><th width="10%">Nature of Offense</th><th width="34%">Offense Narrative</th></tr>';
+		tableString += '<tr><th width="6%">View Details</th><th width="6%">Tracking Number</th><th width="10%">Date of Report</th><th width="10%">Date of Offense</th><th width="12%">Report Completed By</th><th width="10%">Department</th><th width="12%">Nature of Offense</th><th width="34%">Offense Narrative</th></tr>';
 		tableString += '</thead><tbody>';
 
 		var maxLength = this.truncLength;
@@ -34,7 +34,7 @@ dashboard = {
 				truncatedOffenseNarrative = value.OffenseNarrative;
 			}
 
-			tableString += '<tr><td></td><td></td><td>' + value.ReportID + '</td><td>' + value.DateOfReport + '</td><td>' + value.DateOfOffense + '</td><td>' + value.ReportCompletedBy + '</td><td>' + value.Department + '</td><td>' + value.NatureOfOffense + '</td><td id="offense-narrative">' + truncatedOffenseNarrative + '</td></tr>';
+			tableString += '<tr><td></td><td>' + value.ReportID + '</td><td>' + value.DateOfReport + '</td><td>' + value.DateOfOffense + '</td><td>' + value.ReportCompletedBy + '</td><td>' + value.Department + '</td><td>' + value.NatureOfOffense + '</td><td id="offense-narrative">' + truncatedOffenseNarrative + '</td></tr>';
 		});
 
 
