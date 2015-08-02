f0b3ec351ceab7d40d732576493390d121eec993
Jeff <you@example.com>
2015-08-02 11:12:33 -0600

    Add form validation to view init method

--- views/home.js
+++ views/home.js
@@ -6,6 +6,7 @@ home = {
 	init: function() {
 
 		this.bindEvents();
+		this.addFormValidation();
 	},
 
 	render: function() {
@@ -22,8 +23,97 @@ home = {
 
 		$("#incident-report").submit(function(event) {
 
-			alert("S");
+			
 		});
+	},
+
+	addFormValidation: function() {
+
+		alert("adding vaidation");
+		$("#incident-report").validate({
+		   rules: {
+		     dateOfReport: {
+		     	required: true
+		     },
+		     reportCompletedBy: {
+		     	required: true
+		     },
+		     title: {
+		     	required: true
+		     },
+		     department: {
+		     	required: true
+		     },
+		     extension: {
+		     	required: true
+		     },
+		     dateOfOffense: {
+		     	required: true
+		     },
+		     timeOfOffense: {
+		     	required: true
+		     },
+		     natureOfOffense: {
+		     	required: true
+		     },
+		     locationOfOffense: {
+		     	required: true
+		     },
+		     offenseNarrative: {
+		     	required: true
+		     },
+
+		     victimName: {
+
+		     },
+		     victimGender: {
+
+		     },
+		     victimApproximateAge: {
+
+		     },
+		     victimRace: {
+
+		     },
+		     victimUniversityAffiliation: {
+
+		     },
+
+		     suspectName: {
+
+		     },
+		     suspectGender: {
+
+		     },
+		     suspectApproximateAge: {
+
+		     },
+		     suspectRace: {
+
+		     },
+		     suspectUniversityAffiliation: {
+
+		     },
+		     suspectClothing: {
+
+		     },
+		     suspectHair: {
+
+		     },
+		     suspectApproximateHeight: {
+
+		     },
+		     suspectApproximateWeight: {
+
+		     },
+		     suspectFacialHair: {
+
+		     },
+		     otherPhysicalCharacteristics: {
+
+		     },
+		   }
+		 })
 	},
 
 	close: function() {
