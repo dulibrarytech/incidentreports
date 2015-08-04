4271dec0819e39c7d2f9d1f1d08c0dc0707da8c3
Jeff <you@example.com>
2015-08-04 10:50:08 -0600

    Add validation function to login view

--- views/login.js
+++ views/login.js
@@ -11,8 +11,26 @@ login = {
 
 		$('#login-submit').click(function(event) {
             event.preventDefault();
-            systemUtils.submitLoginForm();
+            if(loginView.validate( $('#login-uname').val(), $('#login-passwd').val() )) {
+            	systemUtils.submitLoginForm();
+            }
         });
+	},
+
+	validate: function(uname, passwd) {
+
+		var isValid = true;
+
+		if($('#login-uname').val().length > 20) {
+			$('#login-uname').val("Please enter 20 characters or less");
+			isValid = false;
+		}
+		if($('#login-passwd').val().length > 30) {
+			$('#login-passwd').val("Please enter 30 characters or less");
+			isValid = false;
+		}
+
+		return isValid;
 	},
 
 	reset: function() {
