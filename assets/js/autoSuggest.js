4ca1a0fc39bcd3127ef48e67b04699e078151060
Jeff <you@example.com>
2015-09-22 10:32:14 -0600

    Add autoSuggest.js, add script to end of ir-form template

---
+++ assets/js/autoSuggest.js
@@ -0,0 +1,36 @@
+
+$(function() {
+
+	var url = service_url + "search/complete";	
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    
+    $( "#reportCompletedBy" ).autocomplete({
+      source: availableTags
+    });
+});
+
+	
+//});  // closes document
