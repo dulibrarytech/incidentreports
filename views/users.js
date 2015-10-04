dcb84d695cd0790e99ab712c945528689a9e2d50
Jeff <you@example.com>
2015-10-04 11:02:32 -0600

    Update user data field to 'DUID' on edit form and display table

--- views/users.js
+++ views/users.js
@@ -24,12 +24,13 @@ users = {
 		var tableString = "";
 		var isAdmin;
 		var editLink;
+		var duidString = "";
 
 		// Add new user link
 
 		// Construct header
 		tableString += '<thead>';
-		tableString += '<tr><th width="20%">Username</th><th width="30%">Email</th><th width="10%">Admin</th><th width="10%">Send Type</th><th width="15%"></th><th width="15%"></th></tr>';
+		tableString += '<tr><th width="20%">DUID</th><th width="30%">Email</th><th width="10%">Admin</th><th width="10%">Send Type</th><th width="15%"></th><th width="15%"></th></tr>';
 		tableString += '</thead><tbody>';
 
 		$.each(tableData, function(index, value) {
@@ -37,11 +38,14 @@ users = {
 			// Add human readable value
 			isAdmin = value.isAdmin == '1' ? "Yes" : "No";
 
+			// Don't display "null" in table data
+			duidString = value.DUID == null ? "" : value.DUID;
+
 			// Set crud links with current iteration userID
 			editLink = '<a onclick="userUtils.editUserData(' + value.EmailID + ')">Edit</a>';
 			removeLink = '<a onclick="userUtils.removeUserData(' + value.EmailID + ')">Remove</a>';
 
-			tableString += '<tr><td id="username-' + value.EmailID + '">' + value.LoginID + '</td><td id="email-' + value.EmailID + '">' + value.Email + '</td><td id="admin-' + value.EmailID + '">' + isAdmin + '</td><td id="sendType-' + value.EmailID + '">' + value.SendType + '</td><td>' + editLink + '</td><td>' + removeLink + '</td></tr>';
+			tableString += '<tr><td id="username-' + value.EmailID + '">' + duidString + '</td><td id="email-' + value.EmailID + '">' + value.Email + '</td><td id="admin-' + value.EmailID + '">' + isAdmin + '</td><td id="sendType-' + value.EmailID + '">' + value.SendType + '</td><td>' + editLink + '</td><td>' + removeLink + '</td></tr>';
 		});
 
 		tableString += "</tbody>";
